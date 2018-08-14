cbd93246a0651abd1dc2173550f76fd1a3b02ab6
Jesse Abramowitz <jesseabramowitz@hotmail.com>
2018-08-14 10:19:47 -0400

    optimized the contract

--- truffle box react/contracts/BlockchainForPeace.sol
+++ truffle box react/contracts/BlockchainForPeace.sol
@@ -14,23 +14,27 @@ contract BlockchainForPeace {
     }
     //mapping an address to the donation struct 
     mapping (address => Donation) public donors;
+    event Donate(address indexed from, uint amount, string message);
     
     //constructor to initiate the address of the charity being donated to
     constructor (address _charity) public {
         charity = _charity;
     }
    
-    // payable function which auto trasnfers money to charity address, collects the value and increases the total value counter.
-    function () payable public {
-        charity.transfer(msg.value);
+    // payable function which auto transfers money to charity address, collects the value and increases the total value counter.
+    function fallback() payable public {
         donors[msg.sender].value = msg.value; 
         raised += msg.value;
+        charity.transfer(msg.value);
     }
     // optional message to be sent with donation, peace message.
-    function messageForPeace(string _message) public {
+    function messageForPeace(string _message) payable public {
+        donors[msg.sender].value += msg.value;
         require(donors[msg.sender].value != 0);
         donors[msg.sender].message = _message;
+        charity.transfer(msg.value);
+        raised += msg.value;
+        emit Donate(msg.sender, msg.value, _message);
     }
     
-    event Donate (address indexed from, uint amount);
 }
